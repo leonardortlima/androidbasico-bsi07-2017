2365988dca4bd4afac6316ed8e8a63ec06d0f24e
Leonardo Lima <leonardortlima@gmail.com>
2017-04-26 11:23:45 -0300

    Material da primeira aula

---
+++ Aula1/app/src/main/java/io/leonardortlima/aula1/MainActivity.java
@@ -0,0 +1,23 @@
+package io.leonardortlima.aula1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Aulas de Android básico - Bacharelado em Sistemas de Informação (Semestre 07)
+
+Aulas ministradas para a turma de Sistemas de Informação do sétimo semestre do UNIVEM.
+
+Conceitos básicos do Android:
+
+* Views e ViewGroups
+
+* Explorando as ferramentas disponibilizadas pelas bibliotecas de suporte
