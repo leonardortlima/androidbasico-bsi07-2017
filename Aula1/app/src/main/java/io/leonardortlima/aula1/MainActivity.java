25a57bab0c094f6bbc634aa5f209a6a7d9b88e95
Leonardo Lima <leonardortlima@gmail.com>
2017-04-26 22:41:59 -0300

    Alteracoes em aula

--- Aula1/app/src/main/java/io/leonardortlima/aula1/MainActivity.java
+++ Aula1/app/src/main/java/io/leonardortlima/aula1/MainActivity.java
@@ -3,8 +3,13 @@ package io.leonardortlima.aula1;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -14,10 +19,68 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Criando um menu na toolbar");
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                facaAlgo(id);
+                break;
+
+            case R.id.action_1:
+
+                break;
+
+            case R.id.action_2:
+
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_camera:
+                facaAlgo(id);
+                break;
+
+            case R.id.nav_gallery:
+                break;
+
+            case R.id.nav_slideshow:
+                break;
+        }
+
         return true;
+    }
+
+    public void facaAlgo(int id) {
+        Toast.makeText(this, "Cliquei no item " + id, Toast.LENGTH_LONG).show();
     }
 }
